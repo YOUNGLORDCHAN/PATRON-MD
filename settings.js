e90f048de7339510345b4425946e8b2d11c20ac9
YOUNGLORDCHAN <164238502+YOUNGLORDCHAN@users.noreply.github.com>
2025-01-15 07:07:27 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "8077550666"
 
-global.bankowner = "FADARE"
+global.bankowner = "BRIGHT"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
